145ef5f79987cc7eef020dcc6405cfafa1590821
glitchy <thepatheticweebgamer@gmail.com>
2022-10-13 14:40:09 -0400

    :recycle: Completely New Script
    Rewrote the entire bot to use Gelbooru instead of relying on Reddit to give a bigger variety of images.
    
    Signed-off-by: GlitchyChan <46732589+GlitchChan@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,134 +1,115 @@
-import uuid
-import random
 import asyncio
-import logging
-import asyncprawcore
-import requests
-import tweepy.errors
-
-
-from os import getenv
+import sys
+from distutils.util import strtobool
 from io import BytesIO
+from os import getenv
 from pathlib import Path
-from asyncpraw import Reddit
-from logging import Formatter
+
+from discord_webhook import AsyncDiscordWebhook
 from dotenv import load_dotenv
-from aiohttp import ClientSession
-from asyncpraw.models import Submission
-from tweepy import OAuth1UserHandler, API
-from discord_webhook import DiscordWebhook
-from logging.handlers import TimedRotatingFileHandler
+from httpx import AsyncClient
+from loguru import logger as log
+from pygelbooru import Gelbooru
+from pygelbooru.gelbooru import GelbooruImage
+from tweepy import API, OAuth1UserHandler
 
-
+# Load environment variables
 load_dotenv()
-log = logging.getLogger('moebot')
-handler = TimedRotatingFileHandler(filename=f'{Path(__file__).parent.absolute()}/logs/runtime.log', when='D', interval=1, backupCount=10, encoding='utf-8', delay=False)
-formatter = Formatter(fmt="[%(asctime)s] - [%(levelname)-7s : %(lineno)d]: %(message)s")
-handler.setFormatter(formatter)
-log.addHandler(handler)
 
-# Set logging level
-log.setLevel(logging.INFO)
+# Setup more custom loguru logger
+log.remove()
+log.add(
+    f"{Path(__file__).parent.absolute()}/logs/moebot.log",
+    backtrace=True,
+    enqueue=True,
+    diagnose=True,
+    rotation="00:00",
+    retention="5 days",
+)
+log.add(sink=sys.stdout, level="DEBUG" if strtobool(getenv("DEBUG")) else "INFO", enqueue=True, colorize=True)
+gel = Gelbooru(getenv("GELBOORU_API_KEY"), getenv("GELBOORU_USER_ID"))
+webhook = AsyncDiscordWebhook(url=getenv("WEBHOOK"), rate_limit_retry=True)
+auth = OAuth1UserHandler(
+    getenv("TWITTER_CONSUMER_KEY"),
+    getenv("TWITTER_CONSUMER_SECRET"),
+    getenv("TWITTER_ACCESS_TOKEN"),
+    getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+)
+twit = API(auth)
 
+# Configure what tags you want
+TAGS = []
 
-async def reddit_post(subreddit: str) -> Submission:
-    """
-    Function to get a random post from a given subreddit
-
-    :param subreddit: Subreddit to take posts from
-    :return: A PRAW Submission object
-    """
-    log.info(f"Getting a submission from the subreddit: {subreddit}")
-
-    async with Reddit(
-        client_id=getenv('REDDIT_ID'),
-        client_secret=getenv('REDDIT_SECRET'),
-        user_agent="MoeBot with AsyncPRAW"
-    ) as r:
-        try:
-            sub = await r.subreddit(subreddit)
-            post = random.choice([
-                submission
-                async for submission in sub.hot(limit=30)
-                if not submission.is_self and not submission.url.startswith("https://v.redd.it/")
-            ])
-            log.debug(f"Got post {post.title}")
-            return post
-        except asyncprawcore.exceptions.ResponseException as e:
-            log.exception(f'Reddit gave a {e.response.status}')
-
-
-async def get_image(post: Submission) -> BytesIO:
-    """
-    Function to download an image from a reddit post
-
-    :param post: PRAW Submission object
-    :returns: BytesIO object
-    """
-    log.debug(f"Downloading image for post {post.id}")
-    async with ClientSession() as ses:
-        req = await ses.get(post.url)
-        if req.status < 400:
-            image = BytesIO(await req.content.read())
-            image.name = f"{uuid.uuid4()}.png"
-            log.debug(f"Succesfully got image: {image.name}")
-    return image
-
-
-async def tweet(post: Submission, image: BytesIO) -> None:
-    """
-    Function to post given submission to Twitter Account
-
-    :param post: PRAW Submission object
-    :param image: BytesIO object to send to Twitter
-    """
-    auth = OAuth1UserHandler(
-        getenv('TWITTER_CONSUMER_KEY'),
-        getenv('TWITTER_CONSUMER_SECRET'),
-        getenv('TWITTER_ACCESS_TOKEN'),
-        getenv('TWITTER_ACCESS_TOKEN_SECRET')
-    )
-    t = API(auth)
-    log.debug("Uploading image to Twitter")
-    try:
-        media = t.media_upload(filename=image.name, file=image)
-        t.update_status(status=post.title, media_ids=[media.media_id])
-        log.info("Posted to Twitter")
-    except tweepy.errors.TweepyException as e:
-        log.warning(f"Tweepy gave error {e.__class__}")
+# Configure what tags to exclude
+EXCLUDE_TAGS = [
+    "1boy",
+    "multiple_boys",
+    "furry",
+    "irl",
+    "cosplay",
+    "comic",
+    "meme",
+    "no_humans",
+    "digimon",
+    "bishoujo_senshi_sailor_moon",
+    "adventure_time",
+    "horror",
+    "original",
+    "non-web_source",
+    "kirby_(series)",
+    "pokemon",
+    "rating:explicit",
+    "my_little_pony",
+    "rating:questionable",
+    "feet",
+    "senran_kagura",
+    "monochrome",
+    "very_dark_skin",
+    "user:kamatamaudon",
+    "translation_request",
+    "third-party_edit",
+]
 
 
-async def discord(post: Submission, image: BytesIO) -> None:
-    """
-    Function to post the given submission to Discord webhook
-
-    :param post: PRAW Submission object
-    :param image: BytesIO object to send to Discord
-    """
-    location = image.tell()
-    image.seek(location)
-    hook = DiscordWebhook(getenv('WEBHOOK'), rate_limit_retry=True, content=post.title)
-    hook.add_file(image.getvalue(), image.name)
-    try:
-        hook.execute()
-        log.info("Posted to Discord")
-    except requests.exceptions.HTTPError as e:
-        log.warning(f"Discord Webhook gave error {e.__class__}: {e.response}")
+@log.catch()
+async def download_image(post: "GelbooruImage") -> "BytesIO":
+    log.debug("Downloading Gelbooru image for Twitter")
+    async with AsyncClient() as client:
+        res = await client.get(post.file_url)
+        image = BytesIO(res.content)
+        image.name = f"{post.hash}.{post.file_url.split('.')[-1]}"
+        image.seek(0)
+        log.debug(f"Successfully Downloaded image as: {image.name}")
+        return image
 
 
+@log.catch()
+async def post_discord(image: "GelbooruImage") -> None:
+    log.info("Posting to Discord")
+    webhook.content = image.file_url
+    await webhook.execute()
+    log.success("Successfully posted to Discord")
+
+
+async def post_twitter(image: "BytesIO") -> None:
+    log.info("Posting to Twitter")
+    media = twit.media_upload(image.name, file=image)
+    twit.update_status(status="", media_ids=[media.media_id])
+    log.success("Sucessfully posted to Twitter")
+
+
+@log.catch()
 async def main() -> None:
-    """Entry function to get the bot running"""
-    log.info("Starting loop")
-    subreddits = open('subreddits.txt', 'r').read().splitlines()
-    subreddit = random.choice(subreddits)
+    """Function to start script"""
+    log.info("Starting script...")
+    post: GelbooruImage = await gel.random_post(tags=TAGS, exclude_tags=EXCLUDE_TAGS)
+    log.info("Found Gelbooru image to post")
+    log.debug(f"Fetched gelbooru image: {post.file_url}")
+    image = await download_image(post)
+    await asyncio.gather(post_twitter(image), post_discord(post))
+    log.success("Successfully completed script")
 
-    post = await reddit_post(subreddit)
-    img = await get_image(post)
 
-    await asyncio.gather(
-        discord(post, img),
-        tweet(post, img)
-    )
-
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
